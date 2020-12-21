f9b3605f8368e0525bf9d498ecd397fa43c63f49
lazarev <andrey20010012@gmail.com>
2020-12-21 21:07:51 +0200

    start

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'db' => [
+        'host' => 'yv409220.mysql.tools',
+        'user' => 'yv409220_db',
+        'password' => 'dbZgTuQt',
+        'db' => 'yv409220_db',
+    ],
+];
+
